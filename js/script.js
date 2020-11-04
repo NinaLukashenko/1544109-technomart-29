c3fbaeeb55868436a2d54b5784119cecba7f83f4
NinaLukashenko <nina.lucashenco@gmail.com>
2020-11-04 22:34:42 +0200

    Обработка показа формы обратной связи

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Открытие модального окна с картой
 const mapLink = document.querySelector(".map-link");
 const mapPopup = document.querySelector(".modal-form-map");
 const mapClose = mapPopup.querySelector(".modal-close");
@@ -13,11 +14,34 @@ mapClose.addEventListener('click', (evt) => {
 })
 
 window.addEventListener('keydown', (evt) => {
-  console.log(evt);
-  if (evt === 27) {
+  if (evt.keyCode === 27) {
     if (mapPopup.classList.contains('active')) {
       evt.preventDefault();
       mapPopup.classList.remove('active');
+    }
+  }
+})
+
+// Открытие формы обратной связи
+const feedbackLink = document.querySelector(".feedback-link");
+const feedbackPopup = document.querySelector(".modal-form-feedback");
+const feedbackClose = feedbackPopup.querySelector(".modal-close");
+
+feedbackLink.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  feedbackPopup.classList.add('active')
+})
+
+feedbackClose.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('active');
+})
+
+window.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27) {
+    if (feedbackPopup.classList.contains('active')) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove('active');
     }
   }
 })
