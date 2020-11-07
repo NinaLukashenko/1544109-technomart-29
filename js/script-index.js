80c4fd0e1af680759f88963e77cea8a168583ec3
NinaLukashenko <nina.lucashenco@gmail.com>
2020-11-07 21:23:02 +0200

    Открытие модального окна с корзиной

--- js/script-index.js
+++ js/script-index.js
@@ -125,3 +125,29 @@ servicesButtons.forEach(btn => {
     servicesButtons[activeIndex].classList.add("services-button-active");
   })
 })
+
+// Открытие модального окна с корзиной
+const cartLinks = document.querySelectorAll(".button-purchase");
+const cartPopup = document.querySelector(".modal-form-cart");
+const cartClose = cartPopup.querySelector(".modal-close");
+
+cartLinks.forEach((item) => {
+  item.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    cartPopup.classList.add("active")
+  })
+})
+
+cartClose.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  cartPopup.classList.remove("active");
+})
+
+window.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("active")) {
+      evt.preventDefault();
+      cartPopup.classList.remove("active");
+    }
+  }
+})
